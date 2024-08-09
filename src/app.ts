5fde3504cead5a06709a33bbc4b32f1d304f2e7b
MarwanTarik <marwantarik64@gmail.com>
2024-08-09 18:20:26 +0300

    add chat interface

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(chatRoute);
 
 export default app;
