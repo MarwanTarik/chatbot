bccd93d5c71b201308cfa7c5219c2222de28aa22
MarwanTarik <marwantarik64@gmail.com>
2024-08-10 00:42:04 +0300

    fix cors issue

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import chatRoute from './routes/chat.route';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(express.static("public"));
+app.use(cors({
+  origin:  '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+}));
 
 app.use(chatRoute);
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { Request, Response } from 'express';
-import { botResponse } from './services/chatbot.service';
 import app from './app';
 import config from './config/main.config';
 
